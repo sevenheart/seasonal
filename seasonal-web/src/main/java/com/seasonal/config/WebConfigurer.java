418f32dc69848e4d757ea58fc57681e01235e5a5
luxu <1273090719@qq.com>
2019-09-10 09:44:02 +0800

    1.添加了个人中心页面               2.添加了网络异常处理及页面               3.添加了全局异常处理未完成

--- seasonal-web/src/main/java/com/seasonal/config/WebConfigurer.java
+++ seasonal-web/src/main/java/com/seasonal/config/WebConfigurer.java
@@ -23,7 +23,7 @@ public class WebConfigurer implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //拦截 除了 excludePathList方法中页面的 其他请求
-        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns(excludePathList());
+//        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns(excludePathList());
     }
 
     @Override
@@ -31,18 +31,18 @@ public class WebConfigurer implements WebMvcConfigurer {
 
     }
 
-    public List<String> excludePathList() {
-        List<String> listUrls = new ArrayList<String>();
-
-        listUrls.add("/login");      //登录页
-        listUrls.add("/login.html");      //登录页
-        listUrls.add("/register");   // 注册页
-
-        //网站静态资源
-        listUrls.add("/css/**");
-        listUrls.add("/js/**");
-        listUrls.add("/fonts/**");
-        listUrls.add("/images/**");
-        return listUrls;
-    }
+//    public List<String> excludePathList() {
+//        List<String> listUrls = new ArrayList<String>();
+//
+//        listUrls.add("/login");      //登录页
+//        listUrls.add("/login.html");      //登录页
+//        listUrls.add("/register");   // 注册页
+//
+//        //网站静态资源
+//        listUrls.add("/css/**");
+//        listUrls.add("/js/**");
+//        listUrls.add("/fonts/**");
+//        listUrls.add("/images/**");
+//        return listUrls;
+//    }
 }
