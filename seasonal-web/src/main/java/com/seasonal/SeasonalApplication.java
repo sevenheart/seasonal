505e4b3610889e049814d844c8490d97d4e49dfe
luxu <1273090719@qq.com>
2019-09-11 08:35:11 +0800

    用户信息查询功能 注释了异常处理类

--- seasonal-web/src/main/java/com/seasonal/SeasonalApplication.java
+++ seasonal-web/src/main/java/com/seasonal/SeasonalApplication.java
@@ -3,11 +3,13 @@ package com.seasonal;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootApplication
+
 @MapperScan(basePackages = "com.seasonal.mapper")
 @ServletComponentScan
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class SeasonalApplication {
     public static void main(String[] args) {
         SpringApplication.run(SeasonalApplication.class, args);
