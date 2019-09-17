2c0f626dba3034c3b29310b7f56b487d61f9839f
Strin <790018944@qq.com>
2019-09-17 11:32:09 +0800

    支持jsp页面

--- seasonal-web/src/main/java/com/seasonal/test/TestUpload.java
+++ seasonal-web/src/main/java/com/seasonal/test/TestUpload.java
@@ -1,14 +1,46 @@
 package com.seasonal.test;
 
-import com.seasonal.tencent.TencentUploadUtil;
-
-import java.io.File;
-
 public class TestUpload {
+    public static class ThreadPrinter implements Runnable {
+        private String name;
+        private Object prev;
+        private Object self;
+
+        private ThreadPrinter(String name, Object prev, Object self) {
+            this.name = name;
+            this.prev = prev;
+            this.self = self;
+        }
+
+        @Override
+        public void run() {
+            int count = 10;
+            while (count > 0) {
+                synchronized (prev) {
+                    synchronized (self) {
+                        System.out.print(name);
+                        count--;
+                        self.notifyAll();
+                    }
+                    try {
+                        prev.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        //1.创建文件
-        File file = new File("E:\\640.png");
-        //2.调用方法，传入要在服务器上保存的目录及文件名    和  文件
-        TencentUploadUtil.uploadFile("img/a.png",file);
+        Object a = new Object();
+        Object b = new Object();
+        Object c = new Object();
+        ThreadPrinter pa = new ThreadPrinter("A", a, b);
+        ThreadPrinter pb = new ThreadPrinter("B", b, c);
+        ThreadPrinter pc = new ThreadPrinter("C", c, a);
+        new Thread(pa).start();
+        new Thread(pb).start();
+        new Thread(pc).start();
     }
 }
