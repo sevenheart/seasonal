6a5a3a4f3b84e3e5af15348a84a6b1f7648e50c0
luxu <1273090719@qq.com>
2019-09-29 16:07:25 +0800

    评论分页做成狗

--- seasonal-dao/src/main/java/com/seasonal/repository/CommentRepository.java
+++ seasonal-dao/src/main/java/com/seasonal/repository/CommentRepository.java
@@ -4,15 +4,24 @@ package com.seasonal.repository;
 import com.seasonal.pojo.Comment;
 import com.seasonal.pojo.Responses;
 import net.sf.json.JSONObject;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface CommentRepository {
     /**
      * 查询所有评论根据商品id
      * @return
      */
-    List<JSONObject>  findAllComments(String id);
+   List<JSONObject>  findAllComments(String id,int begin,int limit);
+
+    List<JSONObject> findAllOrderByTime(String id,int begin,int limit);
+    /**
+     * 根据id查中评论是个数
+     * @param id
+     * @return
+     */
+    List<Comment> findAllComments(String id);
 
 
 
@@ -25,7 +34,7 @@ public interface CommentRepository {
     /**
      * 插入一条评论
      * @param comment
-     */
+     **/
     int insertComment(Comment comment);
 
     /**
@@ -35,7 +44,6 @@ public interface CommentRepository {
      * @return
      */
     int addResponse( Responses responses);
-
 
 
 
